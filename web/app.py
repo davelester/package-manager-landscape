d38b45555cf1a42415680ddcf20c7130570eba9e
Dave Lester <dave@davelester.org>
2024-03-31 11:45:22 -0700

    Adds markdown package and updates Contribute page to render CONTRIBUTING.md, rendered as HTML.

--- web/app.py
+++ web/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect
 import toml
 import os
 import sys
+import markdown
 
 app = Flask(__name__)
 
@@ -18,13 +19,18 @@ for category in landscape:
         for item in landscape[category][ecosystems]:
             pmcount+=1
 
+# Convert CONTRIBUTING.md to HTML so it can be rendered at /contribute/ route
+with open("../CONTRIBUTING.md", "r", encoding="utf-8") as input_file:
+    contribute_md = input_file.read()
+    contribute_html = markdown.markdown(contribute_md)
+
 @app.route("/")
 def index():
     return render_template('index.html', landscape=landscape, pmcount=pmcount)
 
 @app.route("/contribute/")
 def contribute():
-    return redirect('https://github.com/davelester/package-manager-landscape/blob/main/CONTRIBUTING.md', code=302)
+    return render_template('contribute.html', content=contribute_html)
 
 if __name__ == '__main__':
     app.run(debug=False)
